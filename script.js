928f040e5efcf1d3365b07fbd1b20857a8e9878a
Mathias Bergquist <berka28@hotmail.com>
2021-01-28 16:22:09 +0100

    Inlämning1-JS

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+class User {
+    constructor(firstname, lastname, email, phonenumber, invoiceadress, zipcode, city){
+        this.firstname = firstname
+        this.lastname = lastname
+        this.email = email
+        this.phonenumber = phonenumber
+        this.invoiceadress = invoiceadress
+        this.zipcode = zipcode
+        this.city = city
+    }
+    get id() {
+        return Date.now.toString()
+    }
+    get displayName(){
+        return `${this.firstname}${this.lastname}`
+    }
+}
 
-$(document).ready(funnction(){
-    
+const user = new User('Mathias', 'Bergquist', 'berka@home', '0702222222', 'Hitvägen 1', '33322', 'Degerfors' )
+console.log(user)
+
+let inputs = document.getElementsByTagName('input')
+
+for (let input of inputs) {
+    input.addEventListener('keyup', e => {
+        if(e.target.value.length < 2) {
+            document.getElementById(`${e.target.id}-error`).innerText = 'För få tecken i ' + e.target.id
+            document.getElementById('add_btn').disabled = true
+        
+        } 
+        else {
+            document.getElementById(`${e.target.id}-error`).innerText = ''
+            document.getElementById('add_btn').disabled = false
+        }
+    })
+}
+$(document).ready(function(){
+
 })
+
+/* 
+  document.getElementById('regform').addEventListener(add_btn, (e) => {
+    e.preventDefault()
+    let errors = []
+
+    for (let element of e.target.elements) {
+        switch(element.tagName) {
+            case 'input':
+                errors.push(validateInput(element))
+                break;
+        }
+    }
+
+    if(!errors.includes(true))
+        window.location.replace("success.php");  
+})
+
+function validateInput(element) {
+    if(element.reguired){
+        if (element.value.length <2){
+            return true
+        }
+    }
+    return false
+}
+ */
